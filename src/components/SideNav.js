d97b16ee21fba049ccffc5ffa7515e94f3ef736f
misgana <misganayoseph@gmail.com>
2019-12-24 17:09:43 +0300

    Sell Dashboard

---
+++ src/components/SideNav.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+import {Link} from "react-router-dom";
+import { Sidenav } from 'rsuite';
+
+
+export default class SideBar extends Component {
+    render() {
+        return (
+            <div>
+                hello from Sidenav page
+            </div>
+        )
+    }
+}
